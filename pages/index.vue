99f4ecbd0630c350e668477bc7202f0145013285
Menna <menna.Elgallad@baianat.com>
2023-03-19 19:02:16 +0200

    all added

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,21 @@
 <template lang="pug">
+.content
+  div(ref="draggableRef" class="draggable")
+  .canvasElement
+      canvas(ref="canvasRef")
 
-.canvasElement
-    canvas(ref="canvasRef")
 </template>
 
 <script setup>
 import gsap from "gsap";
+import { Draggable } from "gsap/Draggable";
 import { fabric } from "fabric";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 const canvasRef = ref(null);
-let fabricCanvas;
+const draggableRef = ref(null);
+let fabricCanvas, draggable, fabricElement;
 onMounted(() => {
+  gsap.registerPlugin(ScrollTrigger, Draggable);
   fabricCanvas = new fabric.Canvas(canvasRef.value, {
     height: 500,
     width: 500,
@@ -32,10 +37,30 @@ onMounted(() => {
   console.log(circle);
   fabricCanvas.add(circle).setActiveObject(circle);
   fabricCanvas.add(circle2);
+
+  draggable = Draggable.create(draggableRef.value, {
+    onDrag: () => {
+      if (fabricElement) {
+        fabricElement.set({
+          left: draggable.x,
+          top: draggable.y,
+        });
+        fabricCanvas.renderAll();
+      }
+    },
+    // onDragEnd: () => {
+    //   fabricElement = new fabric.Rect({
+    //     left: draggable.x,
+    //     top: draggable.y,
+    //     width: draggableRef.value.offsetWidth,
+    //     height: draggableRef.value.offsetHeight,
+    //     fill: "red",
+    //   });
+    //   fabricCanvas.add(fabricElement);
+    // },
+  });
 });
 if (process.client) {
-  gsap.registerPlugin(ScrollTrigger);
-
   gsap.from(".mybuttn", {
     autoAlpha: 0,
     duration: 1.5,
@@ -55,5 +80,13 @@ if (process.client) {
     left: 50%;
     transform: translate(-50%, -50%);
   }
+}
+.draggable {
+  width: 100px;
+  height: 100px;
+  background-color: green;
+  position: absolute;
+  top: 100px;
+  left: 100px;
 }
 </style>
