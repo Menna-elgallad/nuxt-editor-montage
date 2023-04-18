5b9451133b013ecd52849f5278bf7385f39b095a
hesham1221 <hishammohmed12345@gmail.com>
2023-04-18 03:59:23 +0200

    Feat : add logic to slides

--- components/slides.vue
+++ components/slides.vue
@@ -1,23 +1,68 @@
 <template lang="pug">
 .mcontainer.surface-200.p-5
     .slides.py-4
-        .flex.gap-3.mt-3(v-for="(element, index) in state.array" :key="index" :class="{ 'array-item': true, 'show': element.show }") 
+        .flex.gap-3.mt-3(v-for="(element, index) in slideStore.canvasSlides" :key="index" :class="{ 'array-item': true, 'show': element.fabric.id === slideStore.currentCanvas.id }") 
             h4.text-blue-900 0{{index}}
-            .slide
+            img.slide(:src="canvasToImage(element.fabric)" v-if="hasCanvas")
+            //- .sil(v-html="fabricImage" v-if="hasCanvas")
+            .slide(v-else)
     .addslide.flex.justify-content-center.align-items-center.mt-3.flex-column(@click="addnewslide()" )
         Icon(name="bx:layer-plus" class="text-5xl text-blue-900 add" )
         h4.text-blue-900.mt-2 Blank slide
-        
+         
 </template>
 
 <script lang="ts" setup>
+import { useSlide } from "~~/stores/slide";
+import { fabric } from "fabric";
+const fabricImage = ref();
+const hasCanvas = ref(false);
+const slideStore = useSlide();
+let canvas: fabric.Canvas;
+onMounted(() => {
+  canvas = document.getElementById("mycanvas").fabric;
+  const newCanvas = Object.assign(
+    Object.create(fabric.Canvas.prototype),
+    canvas
+  );
+  hasCanvas.value = true;
+  console.log(newCanvas.toSVG());
+  // newCanvas.width = 200;
+  // newCanvas.height = 100;
+
+  fabricImage.value = canvasToImage(canvas);
+  console.log(fabricImage.value);
+
+
+  console.log(fabricImage.value);
+});
 const nextIndex = ref(0);
 const state = reactive({
   array: [{ index: nextIndex.value, show: true }],
 });
 
+function canvasToImage(canvas: fabric.Canvas) {
+  return `data:image/svg+xml;utf8,${encodeURIComponent(canvas.toSVG())}`;
+}
+
 async function addnewslide() {
   // create a new element object with a show property
+  const newCanv = new fabric.Canvas("mycanvas", {
+    id: Date.now(),
+    height: 500,
+    width: 750,
+    backgroundColor: "white",
+    // shadow:1,
+    //@ts-ignore
+    backgroundColorAlpha: 0,
+    borderColor: "black",
+    strokeWidth: 5,
+    hasControls: true,
+  });
+  slideStore.canvasSlides.push({
+    fabric: newCanv,
+    slideNumber: slideStore.canvasSlides.length,
+  });
   nextIndex.value++;
   const newElement = { index: nextIndex.value, show: false };
 
@@ -28,6 +73,12 @@ async function addnewslide() {
 
   console.log(state.array);
 }
+watch(slideStore, () => {
+  hasCanvas.value = false
+  setTimeout(() => {
+    hasCanvas.value = true
+  }, 50);
+});
 </script>
 
 <style scoped lang="scss">
@@ -70,7 +121,7 @@ async function addnewslide() {
   }
 }
 .array-item {
-  opacity: 0;
+  opacity: 1;
   transition: opacity 5s ease-in-out;
 }
 @keyframes animate {
--- stores/canvas.ts
+++ stores/canvas.ts
@@ -1,28 +1,20 @@
 import { defineStore } from "pinia";
 import { fabric } from "fabric";
-import { useLocalStorage } from '@vueuse/core'
+import { useLocalStorage } from "@vueuse/core";
 
 // let mycanvas: fabric.Canvas;
 let selectedElement: Ref<any>;
+let myCanvas: fabric.Canvas;
 
 export const useCanvas = defineStore("canvas", {
-  state: () => (
-    {canasWrapper: "", color: "", canvasref: "" , 
-  selectedElement , selectedProp :"" , selectedPropColor : "" , selectedID :""}),
-
-  // actions: {
-  //   setCanvasElement(canvas: HTMLCanvasElement) {
-  //     this.$state.mycanvas = new fabric.Canvas(canvas, {
-  //       height: 500,
-  //       width: 750,
-  //       backgroundColor: "white",
-  //       // shadow:1,
-  //       //@ts-ignore
-  //       backgroundColorAlpha: 0,
-  //       borderColor: "black",
-  //       strokeWidth: 5,
-  //       hasControls: true,
-  //     });
-  //   },
-  // },
+  state: () => ({
+    canasWrapper: "",
+    color: "",
+    canvasref: "",
+    selectedElement,
+    selectedProp: "",
+    selectedPropColor: "",
+    selectedID: "",
+    myCanvas: myCanvas,
+  }),
 });
